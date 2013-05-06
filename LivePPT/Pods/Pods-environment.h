dffa3b7b70697118a1b7adc9d5e200d7eac0841e
Bowen Liang <simonlbw@gmail.com>
2013-05-07 01:04:32 +0800

    运用Cocoapods增加SocketRocket及其依赖框架，SocketRocket的版本号为v0.2.0

--- LivePPT/Pods/Pods-environment.h
+++ LivePPT/Pods/Pods-environment.h
@@ -12,3 +12,9 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking 2
 #define COCOAPODS_VERSION_PATCH_AFNetworking 1
 
+// SocketRocket
+#define COCOAPODS_POD_AVAILABLE_SocketRocket
+#define COCOAPODS_VERSION_MAJOR_SocketRocket 0
+#define COCOAPODS_VERSION_MINOR_SocketRocket 2
+#define COCOAPODS_VERSION_PATCH_SocketRocket 0
+
