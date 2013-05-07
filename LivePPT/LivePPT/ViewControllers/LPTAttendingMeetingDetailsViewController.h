dc28bd96c7a4bb8ee2f2427b83700b1e4b6442a7
Bowen Liang <simonlbw@gmail.com>
2013-05-08 05:17:24 +0800

    增加单个参与会议的会议详情页面。

---
+++ LivePPT/LivePPT/ViewControllers/LPTAttendingMeetingDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LPTAttendingMeetingDetailsViewController.h
+//  LivePPT
+//
+//  Created by Bowen Liang on 13-5-8.
+//  Copyright (c) 2013年 Fever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPTAttendingMeetingDetailsViewController : UITableViewController
+
+@end
